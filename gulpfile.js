6c854f4f4a8f51f490564c59f9f07a3dc7520fa6
Yusuff Ridwan <wemzain@gmail.com>
2022-04-27 21:00:04 +0100

    we optimized our landing page

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,66 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
-  
-  exports.default = defaultTask
+var gulp = require('gulp');
+var sass = require('gulp-sass')(require('sass'));
+var cleanCSS = require('gulp-clean-css');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+var imagemin = require('gulp-imagemin');
+sass.compiler = require('node-sass');
+
+
+var paths = {
+    styles: {
+      src: 'src/css/style.scss',
+      dest: 'dist'
+    }
+  };
+
+
+
+gulp.task("sass", function() {
+    return gulp.src(paths.styles.src)
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(
+            cleanCSS({
+                compatibility: 'ie8'
+            })
+        )
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(browserSync.stream())
+});
+
+gulp.task("html", function() {
+    return gulp.src("src/*.html")
+        .pipe(gulp.dest("dist"))
+});
+
+
+gulp.task("fonts", function() {
+    return gulp.src("src/fonts/*")
+        .pipe(gulp.dest("dist/fonts"))
+});
+
+
+gulp.task("images", function() {
+    return gulp.src("src/img/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("dist/imgs"))
+});
+
+gulp.task("watch", function() {
+
+    browserSync.init({
+        server: {
+            baseDir: 'dist'
+        }
+    })
+    gulp.watch(paths.styles.src, gulp.series("sass"))
+    gulp.watch("src/index.html", gulp.series("html")).on("change", browserSync.reload)
+    gulp.watch("src/fonts/*", gulp.series("fonts"))
+    gulp.watch("src/img/*", gulp.series("images"))
+});
+
+
+gulp.task('default', gulp.series("html", "sass","fonts","images", "watch"));
+
